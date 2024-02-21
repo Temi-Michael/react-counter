a0e2479994892ee8f189390fc16c54773db1b3f0
Temi Michael <olasehindemichael285@gmail.com>
2024-02-21 01:26:43 +0100

    My first understandable React code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+// const Cars = () => {
+
+//   let list = ['Mercedes', 'Audi', 'BMW', 'Toyota', 'Tesla'];
+
+//   let lists = list.map(item => {
+//     return <li>{ item }</li>
+//   })
+
+//   return (
+//     <ol className="header-para">
+//       { lists }
+//     </ol>
+//   )
+// }
+
+// const App = () => {
+//   return <Cars />;
+// }
+
+
+// const App = () => {
+//   return (
+//     <section>
+//       <button onClick={event => {
+//         console.log("Hello I was clicked")
+//       }}>Click Me</button>
+//     </section>
+//   )
+// }
+
+
+// const App = () => {
+
+//   const handleClick = (event) => {
+//     console.log(`You just clicked me`);
+//   }
+//   return (
+//     <section>
+//       <button onClick={handleClick}>Click Me</button>
+//     </section>
+//   )
+// }
+
+
+const App = () => {
+
+  // State...
+  const [counter, setCounter] = useState(0)
+  //Handle Increase...
+  const increase = (event) => {
+    event.preventDefault();
+    setCounter(prev => prev + 1);
+
+  }
+  //Handle Decrease...
+  const decrease = (event) => {
+    event.preventDefault();
+    setCounter(prev => prev - 1);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    <section>
+      <button className="action-btn" onClick={increase}>Increase</button>
+      <button className="action-btn" onClick={decrease}>Decrease</button>
+      <p className="result">{counter}</p>
+    </section>
+  )
 }
 
 export default App;
